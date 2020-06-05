2459d5652a604d20b503ae1690d42b6f9ea2c6ae
Rodrigo Santos da Silva <rodrigosnt3@gmail.com>
2020-06-04 21:05:44 -0300

    Pequena correçao na sintaxe do index e remoçao de 2 componentes nao utilizados

--- front/src/index.js
+++ front/src/index.js
@@ -30,12 +30,7 @@ ReactDOM.render(
         <Route path="/admin" component={ props=> <AdminLayout {...props} />}/>
         <Route path="/admin/classes" component={ props => <AdminLayout {...props} component={Classes}/>}/>
         <Route path="/admin/classdashboard" render={props => <AdminLayout {...props} component={ClassDashboard}/>}/>
-<<<<<<< HEAD
-        <Route path="/admin/studentdashboard" render={props => <AdminLayout {...props} component={StudentDashboard}/>} />
-        <Route path="/admin/missions" render={props => <AdminLayout {...props} component={Missions}/>}/>
         <Route path="/dashboardQuestions" component={DashboardQuestions}/>
-=======
->>>>>>> f40164dd0404780a954b8d6bcac7fcdd805a9f90
         <Route path="/thanks" component={Thanks}/>
         <Redirect to="/"/>
     </Switch>
