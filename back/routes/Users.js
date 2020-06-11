37646229d6abbbea3ce5a10e22627d810837099b
paodealho404 <phdbnpedro@gmail.com>
2020-06-11 18:19:15 -0300

    Prod build

--- back/routes/Users.js
+++ back/routes/Users.js
@@ -11,32 +11,32 @@ users.use(cors())
 process.env.SECRET_KEY = 'secret';
 
 
-users.post('/login', (req, res) => {
-  user_.findOne({
-    where: {
-      login: req.body.login,
-      password: req.body.password,
-      role: "110"
-    }
-  })
-    .then(user => {
-      if (user) {
-         if (bcrypt.compare(req.body.password, user.password)) {
-          let token = jwt.sign(user.dataValues, process.env.SECRET_KEY, {
-            expiresIn: 1440
-          })
-          res.send(token)
-        }else{
-          res.json({ error: 'Usuário e senha não combinam.' });
-        }
-      } else {
-        res.status(400).json({ error: 'Por favor, digite login e senha corretamente.' });
-      }
-    })
-    .catch(err => {
-      res.status(400).json({ error: err });
-    })
-})
+// users.post('/login', (req, res) => {
+//   user_.findOne({
+//     where: {
+//       login: req.body.login,
+//       password: req.body.password,
+//       role: "110"
+//     }
+//   })
+//     .then(user => {
+//       if (user) {
+//          if (bcrypt.compare(req.body.password, user.password)) {
+//           let token = jwt.sign(user.dataValues, process.env.SECRET_KEY, {
+//             expiresIn: 1440
+//           })
+//           res.send(token)
+//         }else{
+//           res.json({ error: 'Usuário e senha não combinam.' });
+//         }
+//       } else {
+//         res.status(400).json({ error: 'Por favor, digite login e senha corretamente.' });
+//       }
+//     })
+//     .catch(err => {
+//       res.status(400).json({ error: err });
+//     })
+// })
 
 users.get('/profile', (req, res) => {
   var decoded = jwt.verify(req.headers['authorization'], process.env.SECRET_KEY);
