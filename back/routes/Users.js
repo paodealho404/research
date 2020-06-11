ed5eff58baa38e7aa8c0e86e0af40833e4f0c47f
paodealho404 <phdbnpedro@gmail.com>
2020-06-11 18:16:25 -0300

    Prod build

--- back/routes/Users.js
+++ back/routes/Users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const users = express.Router();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 
 const user_ = require('../models').user_;
 
